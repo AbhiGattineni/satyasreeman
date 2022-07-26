f00bd4f152f219bc659c64469ab3f2f3c24754ad
AbhiGattineni <abhishekgattineni@gmail.com>
2022-07-26 11:10:45 -0700

    Changes in Media component

--- src/pages/Media.jsx
+++ src/pages/Media.jsx
@@ -3,15 +3,15 @@ import { data } from "../Components/Media_Data"
 const Media = () => {
     return (
         <div className="h-screen w-full bg-white">
-            <div className="text-3xl font-bold text-center">
+            <div className="m-8 text-3xl font-bold text-center">
                 Media
             </div>
-            <div className="border border-spacing-2 rounded-md m-8 bg-slate-100 p-5">
-                <div className="grid justify-items-center lg:grid-cols-4 md:grid-cols-3 grid-cols-1 gap-5">
+            <div className="m-8">
+                <div className="grid justify-items-center lg:grid-cols-5 md:grid-cols-4 grid-cols-1">
                     {data.map((item) => (
-                        <div className="">
-                            <img className="w-[200px] h-[200px] hover:shadow-2xl hover:scale-110 ease-in-out duration-300" src={item.img} alt=""></img>
-                            <p className="ml-12 py-3">{item.Description}</p>
+                        <div className="hover:shadow-2xl hover:scale-110 ease-in-out duration-1000 mt-5 border-x-2 border-b-2 border-slate-100">
+                            <img className="rounded-t-lg w-[200px] h-[200px]" src={item.img} alt=""></img>
+                            <text className="text-center m-3 p-3 text-xs">{item.Description}</text>
                         </div>
                     ))}
                 </div>
