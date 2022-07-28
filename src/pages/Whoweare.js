431b8512852fe83c3d251ca0468306fd4a771b27
Trinath Gundla <84904193+GundlaTrinath@users.noreply.github.com>
2022-07-28 17:12:51 +0530

    Updated for Mobile view

--- src/pages/Whoweare.js
+++ src/pages/Whoweare.js
@@ -4,17 +4,17 @@ import image2 from "../images/image2.jpg";
 
 const WhoWeAre = () => {
     return (
-        <div className="h-screen w-full bg-slate-100">
+        <div className="w-full bg-slate-100">
             <div className="text-3xl font-bold text-center mt-5">
                 Leading The Way.
             </div>
-            <div class="grid lg:grid-cols-6">
-                <div className="text-sm text-left lg:col-start-3 lg:col-span-2 p-3">
+            <div class="">
+                <div className="text-sm text-left p-3">
                     We believe that the fundamental role of leadership is to create and maintain a culture that strengthens the company's core values, encourages employees to do the right thing, and helps drive the company's long-term strategy. All delivered with humility and compassion.
                 </div>
             </div>
             <div className='border border-1 rounded-md bg-white'>
-                <div className=" md:grid md:grid-cols-1 lg:grid-cols-2">
+                <div className="grid lg:grid-cols-2 grid-cols-1">
                     <Card image={image1} />
                     <Card image={image2} />
                 </div>
